96175b28e51d4ecc9f7494346831087f5fc6c5ff
Woonggi Min <min20000416@naver.com>
2022-01-07 14:56:52 +0900

    user 테이블 컬럼 추가 및 수정

--- basketball_stats/user/admin.py
+++ basketball_stats/user/admin.py
@@ -3,7 +3,10 @@ from .models import BsUser
 
 # Register your models here.
 
+
 class BsUserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'teamname', 'backnumber', 'password' )
+    list_display = ('user_id', 'password', 'user_name',
+                    'team_name', 'back_number')
+
 
 admin.site.register(BsUser, BsUserAdmin)
--- basketball_stats/user/models.py
+++ basketball_stats/user/models.py
@@ -4,14 +4,14 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 class BsUser(models.Model):
-    username = models.CharField(max_length=32, verbose_name='이름')
-    teamname = models.CharField(max_length=32, verbose_name='팀명')
-    backnumber = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(0), MaxValueValidator(100)], verbose_name='등번호')
+    user_id = models.CharField(max_length=32, verbose_name='아이디')
     password = models.CharField(max_length=128, verbose_name='비밀번호')
+    user_name = models.CharField(max_length=32, verbose_name='이름')
+    team_name = models.CharField(max_length=32, verbose_name='팀명')
+    back_number = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(0), MaxValueValidator(100)], verbose_name='등번호')
     registered_dttm = models.DateTimeField(auto_now_add=True, verbose_name='가입일시')
 
     class Meta:
-        managed = False
         db_table = 'bs_user'
         verbose_name = 'bs user'
         verbose_name_plural = 'bs user'
